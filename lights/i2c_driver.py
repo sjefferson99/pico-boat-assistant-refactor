42a8ceb3872a249121e065ba410a12c57e48852d
Stephen Jefferson <stephen@sjefferson.co.uk>
2023-10-13 17:29:45 +0100

    Turn on light 0 with I2C code

--- lights/i2c_driver.py
+++ lights/i2c_driver.py
@@ -1,5 +1,9 @@
 import i2c_responder
+from lights.driver import Lights_Driver
 
 class Lights_I2C_Driver:
     def __init__(self) -> None:
-        pass
+        self.lights_driver = Lights_Driver()
+
+    def process_I2C(self, data):
+        self.lights_driver.light_on(0)
--- tests/lights/test_i2c_lights.py
+++ tests/lights/test_i2c_lights.py
@@ -3,3 +3,10 @@ from lights.i2c_driver import Lights_I2C_Driver
 def test_import_lights_i2c():
     lights_i2c_driver = Lights_I2C_Driver()
     assert isinstance(lights_i2c_driver, Lights_I2C_Driver) == True
+
+def test_turn_on_light_0_via_I2C_data(mocker):
+    light_on_patch = mocker.patch('lights.driver.Lights_Driver.light_on')
+    lights_i2c_driver = Lights_I2C_Driver()
+    lights_i2c_driver.process_I2C(0x00)
+    light_on_patch.assert_called_once_with(0)
+    
