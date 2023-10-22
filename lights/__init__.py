4dd6a09a26ff77f7a52630f5f7bb6e8b2110e51b
Stephen Jefferson <stephen@sjefferson.co.uk>
2023-10-22 23:44:00 +0100

    Light class init i2c driver

--- lights/__init__.py
+++ lights/__init__.py
@@ -1,5 +1,7 @@
 from lights.driver import Lights_Driver
+from lights.i2c_driver import Lights_I2C_Driver
 
 class LightModule:
     def __init__(self) -> None:
         self.light_driver = Lights_Driver()
+        self.i2c_driver = Lights_I2C_Driver()
--- tests/lights/test_lights.py
+++ tests/lights/test_lights.py
@@ -1,5 +1,6 @@
 from lights import LightModule
 from lights.driver import Lights_Driver
+from lights.i2c_driver import Lights_I2C_Driver
 
 def test_import_light_module():
     light_module = LightModule()
@@ -8,3 +9,7 @@ def test_import_light_module():
 def test_light_module_has_light_driver():
     light_module = LightModule()
     assert isinstance(light_module.light_driver, Lights_Driver) == True
+
+def test_light_module_has_i2c_driver():
+    light_module = LightModule()
+    assert isinstance(light_module.i2c_driver, Lights_I2C_Driver) == True
