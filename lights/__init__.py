530edd95fcd62513e71ca525a6101bccefa351b8
Stephen Jefferson <stephen@sjefferson.co.uk>
2023-10-22 23:53:12 +0100

    Add responder configuration to lights class

--- lights/__init__.py
+++ lights/__init__.py
@@ -5,3 +5,6 @@ class LightModule:
     def __init__(self) -> None:
         self.light_driver = Lights_Driver()
         self.i2c_driver = Lights_I2C_Driver()
+
+    def configure_as_responder(self):
+        self.i2c_driver.configure_responder()
--- tests/lights/test_lights.py
+++ tests/lights/test_lights.py
@@ -1,6 +1,7 @@
 from lights import LightModule
 from lights.driver import Lights_Driver
 from lights.i2c_driver import Lights_I2C_Driver
+from i2c.i2c_responder import I2CResponder
 
 def test_import_light_module():
     light_module = LightModule()
@@ -13,3 +14,8 @@ def test_light_module_has_light_driver():
 def test_light_module_has_i2c_driver():
     light_module = LightModule()
     assert isinstance(light_module.i2c_driver, Lights_I2C_Driver) == True
+
+def test_i2c_driver_configures_as_responder():
+    light_module = LightModule()
+    light_module.configure_as_responder()
+    assert isinstance(light_module.i2c_driver.responder, I2CResponder)
