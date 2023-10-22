eb0168d2f5ebd29116dbd48ad87a8f653be0e358
Stephen Jefferson <stephen@sjefferson.co.uk>
2023-10-23 00:11:10 +0100

    Custom i2c responder config in light module

--- lights/__init__.py
+++ lights/__init__.py
@@ -1,10 +1,14 @@
 from lights.driver import Lights_Driver
 from lights.i2c_driver import Lights_I2C_Driver
 
-class LightModule:
+class Light_Module:
     def __init__(self) -> None:
         self.light_driver = Lights_Driver()
         self.i2c_driver = Lights_I2C_Driver()
+        self.i2c_device_id=0
+        self.sda_gpio=16
+        self.scl_gpio=17
+        self.responder_address=0x41
 
-    def configure_as_responder(self):
-        self.i2c_driver.configure_responder()
+    def configure_as_responder(self) -> None:
+        self.i2c_driver.configure_responder(self.i2c_device_id, self.sda_gpio, self.scl_gpio, self.responder_address)
--- tests/lights/test_lights.py
+++ tests/lights/test_lights.py
@@ -1,21 +1,34 @@
-from lights import LightModule
+from lights import Light_Module
 from lights.driver import Lights_Driver
 from lights.i2c_driver import Lights_I2C_Driver
 from i2c.i2c_responder import I2CResponder
 
 def test_import_light_module():
-    light_module = LightModule()
-    assert isinstance(light_module, LightModule) == True
+    light_module = Light_Module()
+    assert isinstance(light_module, Light_Module) == True
 
 def test_light_module_has_light_driver():
-    light_module = LightModule()
+    light_module = Light_Module()
     assert isinstance(light_module.light_driver, Lights_Driver) == True
 
 def test_light_module_has_i2c_driver():
-    light_module = LightModule()
+    light_module = Light_Module()
     assert isinstance(light_module.i2c_driver, Lights_I2C_Driver) == True
 
 def test_i2c_driver_configures_as_responder():
-    light_module = LightModule()
+    light_module = Light_Module()
     light_module.configure_as_responder()
     assert isinstance(light_module.i2c_driver.responder, I2CResponder)
+
+def test_i2c_driver_configures_as_custom_responder():
+    light_module = Light_Module()
+    light_module.i2c_device_id=1
+    light_module.sda_gpio=4
+    light_module.scl_gpio=5
+    light_module.responder_address=0x42
+    light_module.configure_as_responder()
+    assert isinstance(light_module.i2c_driver.responder, I2CResponder)
+    assert light_module.i2c_driver.responder.i2c_device_id == 1
+    assert light_module.i2c_driver.responder.sda_gpio == 4
+    assert light_module.i2c_driver.responder.scl_gpio == 5
+    assert light_module.i2c_driver.responder.responder_address == 0x42
